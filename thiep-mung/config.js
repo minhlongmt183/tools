e221c9a5a007f4b94cb90b222e8b80c196c70463
Vo Minh Long <minhlongv84@gmail.com>
2023-04-15 19:25:11 +0700

    add thiep-mung
    add thiep-mung

---
+++ thiep-mung/config.js
@@ -0,0 +1,9 @@
+const nameFriend = 'Long\'s Graduation Day';
+const giftUrl = 'card.png';
+const eventName = 'You are invited';
+const titleCard = 'Long\'s Graduation Day';
+const contentCard = '';
+
+const giftImage = 'card.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
